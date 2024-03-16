13335ce4f30e844a17716570189b2326d457ac6e
Renan <84106732+Renanzinhu@users.noreply.github.com>
2024-03-16 13:34:11 -0300

    Delete Python/1043.py

--- Python/1043.py
+++
@@ -1,9 +0,0 @@
-n = input().split()
-a, b, c = n
-a = float(a)
-b = float(b)
-c = float(c)
-if abs(b - c) < a < (b + c) and (a - c) < b < (a + c) and (a - b) < c < (a + b):
-    print("Perimetro = {:.1f}".format(a + b + c))
-else:
-    print("Area = {:.1f}".format(((a + b) / 2) * c))
