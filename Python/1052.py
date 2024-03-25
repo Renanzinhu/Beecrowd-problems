2e41d61b56b6f31b9c66b7e2f8f19e746c3b5786
Renan <84106732+Renanzinhu@users.noreply.github.com>
2024-03-25 01:43:17 -0300

    Create 1052.py

---
+++ Python/1052.py
@@ -0,0 +1,5 @@
+n = int(input())
+m = ['January','February','March','April','May','June','July','August','September','October','November','December']
+if n>=1 and n<=12:
+    nm = n-1
+    mn = (m[nm])
